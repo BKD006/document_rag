8907cee2d383801f17b8d90ae4edfdd69b54909e
Biro Kishore Das <birokishoredas@gmail.com>
2025-10-18 23:15:19 +0530

    data_ingestion and retrieval pipeline has been updated for single document chat

--- src/single_document_chat/retrieval.py
+++ src/single_document_chat/retrieval.py
@@ -15,16 +15,41 @@ from prompt.prompt_library import PROMPT_REGISTRY
 from model.models import PromptType
 
 class ConversationalRAG:
-    def __init__(self, session_id: str, retriver)->None:
+    def __init__(self, session_id: str, retriever):
         try:
             self.log= CustomLogger().get_logger(__name__)
+            self.session_id=session_id
+            self.retriever=retriever
+            self.llm=self._load_llm()
+            self.contextualize_prompt= PROMPT_REGISTRY[PromptType.CONTEXTUALIZE_QUESTION.value]
+            self.qa_prompt= PROMPT_REGISTRY[PromptType.CONTEXT_QA.value]
+            self.history_aware_retriever= create_history_aware_retriever(
+                self.llm, self.retriever, self.contextualize_prompt
+            )
+            self.log.info("Created history-aware retriever", session_id=session_id)
+            self.qa_chain=create_stuff_documents_chain(self.llm, self.qa_prompt)
+            self.rag_chain= create_retrieval_chain(self.history_aware_retriever, self.qa_chain)
+            self.log.info("Initialized ConversationalRAG", session_id=session_id)
+
+            self.chain=RunnableWithMessageHistory(self.rag_chain,
+                                                  self._get_session_history(),
+                                                  input_messages_key="input",
+                                                  history_messages_key="chat_history",
+                                                  output_messages_key="answer"
+                                                  )
+            self.log.info("Created RunnableWithMessageHistory", session_id=session_id)
+
+
         except Exception as e:
             self.log.error("Unable to initiate ConverstaionalRAG", error=str(e))
             raise DocumentalRagException("Failed to initialize ConversationalRAG", sys)
     
     def _load_llm(self):
         try:
-            pass
+            llm=ModelLoader().load_llm()
+            self.log.info("LLM Loaded successfully", class_name=llm.__class__.__name__)
+            return llm
+
         except Exception as e:
             self.log.error("Error loading LLM via ModelLoader", error=str(e))
             raise DocumentalRagException("Failed to LLM", sys)
@@ -36,16 +61,31 @@ class ConversationalRAG:
             self.log.error("Failed to access session history", session_id=session_id, error=str(e))
             raise DocumentalRagException("Failed to retrieve session history", sys)
     
-    def load_retriever_from_faiss(self):
+    def load_retriever_from_faiss(self, index_path:str):
         try:
-            pass
+            embeddings= ModelLoader().load_embeddings()
+            if not os.path.isdir(index_path):
+                raise FileNotFoundError(f"FAISS index directory not found: {index_path}")
+            vectorstore= FAISS.load_local(index_path, embeddings)
+            self.log.info("Loaded retriver from FAISS index", index_path=index_path)
+            return vectorstore.as_retriever(search_type="similarity", search_kwargs={"k":5})
+
         except Exception as e:
             self.log.error("Failed to load retriever from FAISS", sys(e))
             raise DocumentalRagException("Failed to load retriever from FAISS", sys)
         
-    def invoke(self):
+    def invoke(self, user_input:str)-> str:
         try:
-            pass
+            response=self.chain.invoke(
+                {"input":user_input},
+                config={"configurable": {"session_id":self.session_id}}
+            )
+            answer= response.get("answer", "No answer")
+            if not answer:
+                self.log.warning("Empty answer received", session_id=self.session_id)
+            self.log.info("Chain invoked sucessfully", session_id=self.session_id,user_input=user_input, answer_preview=answer[:158]) 
+            return answer
+
         except Exception as e:
             self.log.error("Failed to invoke conversational RAG", error=str(e), session_id=self.session_id)
             raise DocumentalRagException("Failed to invoke RAG chain", sys)
